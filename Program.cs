dc0e2de891a0b0b03aa2e559a4d2abb6fa850ddb
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-10-14 00:04:54 +0200

    int

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using Google.Ads.Gax.Config;
+using Google.Ads.GoogleAds;
+using Google.Ads.GoogleAds.Config;
+using Google.Ads.GoogleAds.Lib;
+using Google.Ads.GoogleAds.V21.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,6 +17,19 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+// GoogleAdsConfig config = new GoogleAdsConfig()
+// {
+//     DeveloperToken = "",
+//     OAuth2Mode = OAuth2Flow.SERVICE_ACCOUNT
+// };
+
+// // Initialize a GoogleAdsClient class.
+// GoogleAdsClient client = new GoogleAdsClient(config);
+
+// // Create the required service.
+// CampaignServiceClient campaignService =
+//     client.GetService(Services.V21.CampaignService);
 
 app.UseHttpsRedirection();
 
