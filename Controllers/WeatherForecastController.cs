d91635289cdb0cd15d1d066132094967f8c1cc8d
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-12-11 22:51:53 +0100

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,6 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
+    
         "Freezing", "Bracing", "Chilly", "Cool", "Mild",
         "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
